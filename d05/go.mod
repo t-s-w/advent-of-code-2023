de7e8f955791fe2998ce4fb811d8709b8a7ca74a
amphipath <amphipath@users.noreply.github.com>
2023-12-09 15:22:53 +0800

    start day 5 ~*tidily*~

--- d05/go.mod
+++ d05/go.mod
@@ -1,3 +1,11 @@
 module main
 
-go 1.21.1
+go 1.21.3
+
+toolchain go1.21.5
+
+replace github.com/t-s-w/advent-of-code-2023/utils => ../utils
+
+require github.com/t-s-w/advent-of-code-2023/utils v0.0.0-20231209070949-8cb09bc822e6
+
+require github.com/joho/godotenv v1.5.1 // indirect
---
+++ d05/script.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/t-s-w/advent-of-code-2023/utils"
+)
+
+func Part01(input []string) (result int) {
+	return result
+}
+
+func Part02(input []string) (result int) {
+	return result
+}
+
+func main() {
+	input := utils.GetInput(5)
+	fmt.Println(Part01(input))
+	fmt.Println(Part02(input))
+}
